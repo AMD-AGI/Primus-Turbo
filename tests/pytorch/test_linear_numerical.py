15e9443f31c99168e738086f3791481c08fa7317
beinggod <beinggod@foxmail.com>
2025-06-11 07:41:14 +0000

    fix

--- tests/pytorch/test_linear_numerical.py
+++ tests/pytorch/test_linear_numerical.py
@@ -11,55 +11,65 @@ from tests.test_utils import (
 
 
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float16, torch.bfloat16])
-@pytest.mark.parametrize("shapes", [(512, 128, 256)])
-def test_linear_numerical(dtype, shapes):
+@pytest.mark.parametrize("shapes", [(512, 128, 256),
+                                    (8192, 8192, 8192),
+                                    (1, 2048, 128)])
+def test_gemm_numerical(dtype, shapes):
     if not torch.cuda.is_available():
         pytest.skip("CUDA not available")
     torch.manual_seed(42)
 
-    seq_len, in_features, out_features = shapes
+    m, n, k = shapes
     device = "cuda"
 
-    torch_linear = torch.nn.Linear(in_features, out_features, device=device, dtype=dtype)
-    ref_linear = torch.nn.Linear(in_features, out_features, device="cpu", dtype=torch.float32)
-    with torch.no_grad():
-        ref_linear.weight.copy_(torch_linear.weight.cpu())
+    a = torch.randn(m, k, device=device, dtype=dtype, requires_grad=True)
+    a_cpu = a.float().detach().clone().cpu().requires_grad_()
 
-    x1 = torch.randn(seq_len, in_features, device=device, dtype=dtype, requires_grad=True)
-    x2 = x1.float().detach().clone().cpu().requires_grad_()
+    b = torch.randn(n, k, device=device, dtype=dtype, requires_grad=True)
+    b_cpu = b.float().detach().clone().cpu().requires_grad_()
 
-    # === Forward ===
-    out = torch_linear(x1)
-    ref = ref_linear(x2)
+    out = torch.matmul(a, b.T)
+    out = out.cpu()
+    ref = torch.matmul(a_cpu, b_cpu.T)
 
-    # === Backward ===
-    out.backward(torch.ones_like(out))
-    ref.backward(torch.ones_like(ref))
-
-    out = out.float().cpu()
-    grad_output = x1.grad.float().cpu()
-    grad_output_ref = x2.grad.float().cpu()
-
-    grad_weight = torch_linear.weight.grad.float().detach().clone().cpu()
-    grad_weight_ref = ref_linear.weight.grad
-
-    print(f"\n[Linear] dtype={dtype}, shape={shapes}, output result:")
+    print(f"\n[GEMM] dtype={dtype}, shape={shapes}, result:")
     print(f"RelError:   {relative_error(ref, out):.3e}")
     print(f"MAE:        {max_abs_error(ref, out):.3e}")
     print(f"MSE:        {mean_squared_error(ref, out):.3e}")
     print(f"CosSim:     {cosine_similarity(ref, out):.6f}")
     print(f"L2 Norm:    {l2_norm(ref, out):.3e}")
 
-    print(f"\n[Linear] dtype={dtype}, shape={shapes}, grad output result:")
-    print(f"RelError:   {relative_error(grad_output_ref, grad_output):.3e}")
-    print(f"MAE:        {max_abs_error(grad_output_ref, grad_output):.3e}")
-    print(f"MSE:        {mean_squared_error(grad_output_ref, grad_output):.3e}")
-    print(f"CosSim:     {cosine_similarity(grad_output_ref, grad_output):.6f}")
-    print(f"L2 Norm:    {l2_norm(grad_output_ref, grad_output):.3e}")
 
-    print(f"\n[Linear] dtype={dtype}, shape={shapes}, grad weight result:")
-    print(f"RelError:   {relative_error(grad_weight_ref, grad_weight):.3e}")
-    print(f"MAE:        {max_abs_error(grad_weight_ref, grad_weight):.3e}")
-    print(f"MSE:        {mean_squared_error(grad_weight_ref, grad_weight):.3e}")
-    print(f"CosSim:     {cosine_similarity(grad_weight_ref, grad_weight):.6f}")
-    print(f"L2 Norm:    {l2_norm(grad_weight_ref, grad_weight):.3e}")
+@pytest.mark.parametrize("dtype", [torch.float8_e4m3fnuz])
+@pytest.mark.parametrize("shapes", [(512, 128, 256),
+                                    (8192, 8192, 8192),
+                                    (1, 2048, 128)])
+def test_fp8_gemm_numerical(dtype, shapes):
+    if not torch.cuda.is_available():
+        pytest.skip("CUDA not available")
+    torch.manual_seed(42)
+
+    m, n, k = shapes
+    device = "cuda"
+
+    a = torch.randn(m, k, device=device, requires_grad=True)
+    a_cpu = a.float().detach().clone().cpu().requires_grad_()
+    scale_a = torch.tensor(1.0, dtype=torch.float32, device=device)
+    a = a.to(dtype)
+
+    b = torch.randn(n, k, device=device, requires_grad=True)
+    b_cpu = b.float().detach().clone().cpu().requires_grad_()
+    scale_b = torch.tensor(1.0, dtype=torch.float32, device=device)
+    b = b.to(dtype)
+
+    out = torch._scaled_mm(a, b.T, scale_a=scale_a, scale_b=scale_b, out_dtype=torch.float32)
+    out = out.cpu()
+    ref = torch.matmul(a_cpu, b_cpu.T)
+
+    print(f"\n[GEMM] dtype={dtype}, shape={shapes}, result:")
+    print(f"RelError:   {relative_error(ref, out):.3e}")
+    print(f"MAE:        {max_abs_error(ref, out):.3e}")
+    print(f"MSE:        {mean_squared_error(ref, out):.3e}")
+    print(f"CosSim:     {cosine_similarity(ref, out):.6f}")
+    print(f"L2 Norm:    {l2_norm(ref, out):.3e}")
+ 
