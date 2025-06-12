01fa0b8569f17d119b0ad2f2e06267f67801bcfd
liyingli <liyingli@amd.com>
2025-06-12 11:31:57 +0000

    dump and compare nv with amd

--- tests/pytorch/test_special_functions.py
+++ tests/pytorch/test_special_functions.py
@@ -1,13 +1,19 @@
 import pytest
 import torch
 
-from tests.test_utils import (
-    cosine_similarity,
-    max_abs_error,
-    mean_squared_error,
-    relative_error,
-    ulp_error,
+from tests.utils.numerical_utils import (
+    get_device_name,
+    get_device_type,
+    get_file_path,
+    get_format_name,
+    get_subdir,
+    post_process,
+    merge_excels,
+    save_result_to_excel,
+    load_tensor,
+    dump_tensor,
 )
+
 
 FUNC_TABLE = {
     "exp": lambda x: torch.exp(x),
@@ -17,7 +23,8 @@ FUNC_TABLE = {
     "tanh": lambda x: torch.tanh(x),
     "pow": lambda x, y: torch.pow(x.abs() + 1e-3, y.abs()),
 }
-results = []
+
+results, load_results = [], []
 
 
 @pytest.mark.parametrize("func_name", FUNC_TABLE.keys())
@@ -26,11 +33,8 @@ results = []
 def test_special_function_accuracy(func_name, dtype, shape):
     torch.manual_seed(0)
     device = torch.device("cuda")
-    device_name = (
-        torch.cuda.get_device_name(0).split()[2]
-        if torch.version.hip is not None
-        else torch.cuda.get_device_name(0).split()[1]
-    )
+    device_name = get_device_name()
+    device_type = get_device_type()
 
     x = torch.randn(*shape, device=device).to(dtype)
 
@@ -42,45 +46,53 @@ def test_special_function_accuracy(func_name, dtype, shape):
         ref = FUNC_TABLE[func_name](x.cpu())
         out = FUNC_TABLE[func_name](x)
 
-    out = out.float().cpu()
-    ref = ref.float()
+    out = out.cpu()
+    ref = ref
 
-    ulp = ulp_error(out, ref)
+    save_dir = get_subdir()
+    device_type_load = get_device_type(is_load=True)
+    out_load = load_tensor(save_dir, device_type_load, func_name, dtype, shape)
 
-    print(f"\n[{func_name.upper()}] dtype={dtype}, shape={shape}")
-    print(f"RelError:   {relative_error(ref, out):.3e}")
-    print(f"MaxAbsErr:  {max_abs_error(ref, out):.3e}")
-    print(f"MSE:        {mean_squared_error(ref, out):.3e}")
-    print(f"CosSim:     {cosine_similarity(ref, out):.6f}")
-    print(f"ULP(max):   {ulp.max().item()}, ULP(mean): {ulp.float().mean().item():.2f}")
+    dump_tensor(out, save_dir, device_type, func_name, dtype, shape)
 
-    results.append(
-        {
-            "func": f"{func_name.upper()} ({device_name} vs CPU)",
-            "dtype": str(dtype).split(".")[-1],
-            "shape": str(shape),
-            "RelError": f"{relative_error(ref, out):.3e}",
-            "MaxAbsErr": f"{max_abs_error(ref, out):.3e}",
-            "MSE": f"{mean_squared_error(ref, out):.3e}",
-            "CosSim": f"{cosine_similarity(ref, out):.6f}",
-            "ULP_max": f"{ulp.max().item()}",
-            "ULP_mean": f"{ulp.float().mean().item():.2f}",
-        }
-    )
-    if func_name == "pow":
-        results.append(
-            {
-                k: ""
-                for k in [
-                    "func",
-                    "dtype",
-                    "shape",
-                    "RelError",
-                    "MaxAbsErr",
-                    "MSE",
-                    "CosSim",
-                    "ULP_max",
-                    "ULP_mean",
-                ]
-            }
+    if out_load is not None:
+        post_process(
+            get_device_name(is_load=True),
+            device_name,
+            func_name,
+            dtype,
+            shape,
+            out,
+            out_load,
+            load_results,
         )
+
+    post_process("CPU", device_name, func_name, dtype, shape, out, ref, results)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def finalize_results_on_exit(request):
+    def finalizer():
+        save_dir = get_subdir()
+        if load_results:
+            load_device_type = get_device_type(is_load=True)
+            load_file_path = get_file_path(save_dir, get_format_name(load_device_type))
+            save_result_to_excel(load_results, load_file_path)
+
+        if results:
+            device_type = get_device_type()
+            file_path = get_file_path(save_dir, get_format_name(device_type))
+            save_result_to_excel(results, file_path)
+
+        amd_file = save_dir / "AMD_special_func.xlsx"
+        nv_file = save_dir / "NVIDIA_special_func.xlsx"
+        comp_file = save_dir / "GPU_special_func.xlsx"
+
+        merge_files = []
+        for file in [amd_file, nv_file, comp_file]:
+            if file.exists():
+                merge_files.append(file)
+        if len(merge_files) > 1:
+            merge_excels(merge_files, save_dir)
+
+    request.addfinalizer(finalizer)
