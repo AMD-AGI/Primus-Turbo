a1e8c6424efe7eaf98c6f18a2f94b4d44a1d91ef
zhenhuang12 <Zhen.Huang@amd.com>
2025-06-13 05:52:21 +0000

    feat(async_tp): add doc-string for fused_all_gather_matmul.

--- primus_turbo/pytorch/ops/async_tp.py
+++ primus_turbo/pytorch/ops/async_tp.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple
 import torch
 import torch.distributed.distributed_c10d as c10d
 
-from ..kernels.async_tp.ag_gemm_impl import _pipeline_fused_all_gather_matmul_impl
+from primus_turbo.pytorch.kernels.async_tp import ag_gemm_impl
 
 __all__ = ["fused_all_gather_matmul"]
 
@@ -27,6 +27,41 @@ def fused_all_gather_matmul(
     outputs: Optional[List[torch.Tensor]] = None,
     out_dtypes: Optional[List[torch.dtype]] = None,
 ) -> Tuple[Optional[torch.Tensor], List[torch.Tensor]]:
+    """
+    Perform the following logic with micro-pipelined computation and
+    communication:
+        A = all_gather_tensor(A_shard, gather_dim, group_name)
+        C_0 = torch.matmul(A, B_0)
+        C_1 = torch.matmul(A, B_1)
+        C_2 = torch.matmul(A, B_2)
+
+    Optimal stride order for A_shard - if A_shard.movedim(gather_dim, 0) is
+    contiguous, no extra copy is required for input layout transformation.
+    Otherwise A_shard needs to be copied once.
+
+    Parameters:
+        A_shard (torch.Tensor): local sharded input tensor of gemm.
+        Bs (List[torch.Tensor]): a list of local sharded weight tensor of gemm.
+        layouts (List[str]): the layout of A B tensor 'NN' or 'NT' or 'TN'
+        gather_dim (int): A_shard's gather dim
+        group_name (str): tp group's name
+        gemm_streams (List[torch.cuda.Stream]): multi streams for gemm
+        comm_streams (List[torch.cuda.Stream]): multi streams for all gather
+        copy_streams (List[torch.cuda.Stream]): multi streams for copy
+
+    Keyword Arguments:
+        comm_method (str, optional): specify internal algorithm of the implementation, 'ring_exchange' or 'pipeline' or 'auto'. Defaults to "pipeline".
+        num_splits (int, optional): number of chunk splits by pipeline method. Defaults to 2.
+        skip_copy_local_A (bool, optional): skip copy A_shard to A_out, only used for transformer engine. Defaults to False.
+        return_A (bool, optional): if return_A is True, return the result of all-gathered A_shard. Defaults to True.
+        A_out (Optional[torch.Tensor], optional): the output of all-gathered A_shard, Defaults to None.
+        outputs (Optional[List[torch.Tensor]], optional): the output tensors of matmul, Defaults to None.
+        out_dtypes (Optional[List[torch.dtype]], optional): the output dtype of matmul. Defaults to None.
+
+    Returns:
+        Tuple[Optional[torch.Tensor], List[torch.Tensor]]: all-gathered A_shard and output tensors of matmul.
+    """
+
     # check input
     if A_shard.dim() < 2:
         raise ValueError("A_shard must be a matrix")
@@ -81,7 +116,7 @@ def fused_all_gather_matmul(
         raise NotImplementedError()
     else:
         with torch.profiler.record_function("pipeline_fused_all_gather_matmul"):
-            _pipeline_fused_all_gather_matmul_impl(
+            ag_gemm_impl._pipeline_fused_all_gather_matmul_impl(
                 A_shard_flat,
                 Bs,
                 torch.ops.aten.mm,
