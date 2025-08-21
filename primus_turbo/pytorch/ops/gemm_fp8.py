94504cd1d75fb9df23c5be32341e439bb25de637
xiaobochen-amd <xiaobo.chen@amd.com>
2025-08-21 12:45:02 +0000

    feat: gemm fp8 tensorwise support diff layouts

--- primus_turbo/pytorch/ops/gemm_fp8.py
+++ primus_turbo/pytorch/ops/gemm_fp8.py
@@ -233,30 +233,33 @@ class TensorwiseFP8GemmFunction(torch.autograd.Function):
     @staticmethod
     def forward(
         ctx,
-        x: torch.Tensor,
-        y: torch.Tensor,
+        a: torch.Tensor,
+        b: torch.Tensor,
+        trans_a: bool,
+        trans_b: bool,
         out_dtype: torch.dtype,
         config: Float8QuantConfig,
     ):
         assert config.granularity == ScalingGranularity.TENSORWISE
         assert config.format == Format.E4M3 or config.format == Format.HYBRID
 
-        x_dtype = float8_e4m3
-        y_dtype = float8_e4m3
+        a_dtype = float8_e4m3
+        b_dtype = float8_e4m3
 
-        x_scale, x_scale_inv = calc_scale_and_scale_inv(x, torch.finfo(x_dtype).max)
-        y_scale, y_scale_inv = calc_scale_and_scale_inv(y, torch.finfo(y_dtype).max)
+        a_scale, a_scale_inv = calc_scale_and_scale_inv(a, torch.finfo(a_dtype).max)
+        b_scale, b_scale_inv = calc_scale_and_scale_inv(b, torch.finfo(b_dtype).max)
 
-        x_fp8 = quant_fp8_tensorwise_impl(x, x_scale, x_dtype)
-        y_fp8 = quant_fp8_tensorwise_impl(y, y_scale, y_dtype)
+        a_fp8 = quant_fp8_tensorwise_impl(a, a_scale, a_dtype)
+        b_fp8 = quant_fp8_tensorwise_impl(b, b_scale, b_dtype)
 
         # NN
         out = gemm_fp8_tensorwise_impl(
-            x_fp8, x_scale_inv, False, y_fp8.T.contiguous(), y_scale_inv, True, out_dtype, False
+            a_fp8, a_scale_inv, trans_a, b_fp8, b_scale_inv, trans_b, out_dtype, False
         )
 
-        ctx.save_for_backward(x_fp8, x_scale_inv, y_fp8, y_scale_inv)
-
+        ctx.save_for_backward(a_fp8, a_scale_inv, b_fp8, b_scale_inv)
+        ctx.trans_a = trans_a
+        ctx.trans_b = trans_b
         ctx.out_dtype = out_dtype
         ctx.config = config
 
@@ -264,7 +267,7 @@ class TensorwiseFP8GemmFunction(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_out: torch.Tensor):
-        (x_fp8, x_scale_inv, y_fp8, y_scale_inv) = ctx.saved_tensors
+        (a_fp8, a_scale_inv, b_fp8, b_scale_inv) = ctx.saved_tensors
         config = ctx.config
 
         # For HYBRID format. data type of grad_out is e5m2.
@@ -277,30 +280,30 @@ class TensorwiseFP8GemmFunction(torch.autograd.Function):
         grad_out_fp8 = quant_fp8_tensorwise_impl(grad_out, grad_out_scale, grad_out_dtype)
 
         # NT
-        x_grad = gemm_fp8_tensorwise_impl(
+        a_grad = gemm_fp8_tensorwise_impl(
             grad_out_fp8,
             grad_out_scale_inv,
             False,
-            y_fp8,
-            y_scale_inv,
-            True,
+            b_fp8,
+            b_scale_inv,
+            not ctx.trans_b,
             ctx.out_dtype,
-            False,
+            ctx.trans_a,
         )
 
         # TN
-        y_grad = gemm_fp8_tensorwise_impl(
-            x_fp8.T.contiguous(),
-            x_scale_inv,
-            False,
-            grad_out_fp8.T.contiguous(),
+        b_grad = gemm_fp8_tensorwise_impl(
+            a_fp8,
+            a_scale_inv,
+            not ctx.trans_a,
+            grad_out_fp8,
             grad_out_scale_inv,
-            True,
-            ctx.out_dtype,
             False,
+            ctx.out_dtype,
+            ctx.trans_b,
         )
 
-        return (x_grad, y_grad, None, None)
+        return (a_grad, b_grad, None, None, None, None)
 
 
 def gemm_fp8_tensorwise(
@@ -312,8 +315,6 @@ def gemm_fp8_tensorwise(
     config: Union[Float8QuantConfig, None] = None,
 ) -> torch.Tensor:
     assert a.ndim == 2 and b.ndim == 2, "Only 2D tensors are supported"
-    # TODO(ruibzhan): We only support NN layout on gfx942.
-    assert not trans_a and not trans_b, f"Only support NN layout on FP8 tensorwise GEMM."
 
     if out_dtype is None:
         out_dtype = torch.result_type(a, b)
@@ -321,6 +322,6 @@ def gemm_fp8_tensorwise(
     if config is None:
         config = Float8QuantConfig()
 
-    args = (a, b, out_dtype, config)
+    args = (a, b, trans_a, trans_b, out_dtype, config)
 
     return TensorwiseFP8GemmFunction.apply(*args)
--- tests/pytorch/ops/test_gemm_fp8.py
+++ tests/pytorch/ops/test_gemm_fp8.py
@@ -82,46 +82,53 @@ def test_gemm_fp8_blockwise_func(dtype, block_size, B, M, NK):
 @pytest.mark.parametrize("m", [256, 512, 1024, 2048])
 @pytest.mark.parametrize("n", [512, 1024, 2048, 4096])
 @pytest.mark.parametrize("k", [255, 512, 1024, 2048])
+@pytest.mark.parametrize("layout", ["TN", "NN", "NT"])
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16])
-def test_gemm_fp8_tensorwise(m, n, k, dtype):
+def test_gemm_fp8_tensorwise(m, n, k, layout, dtype):
     device = "cuda:0"
 
-    x = torch.randn((m, k), dtype=dtype, device=device, requires_grad=True)
-    w = torch.randn((k, n), dtype=dtype, device=device, requires_grad=True)
-    x_ref = x.detach().clone().requires_grad_()
-    w_ref = w.detach().clone().requires_grad_()
+    trans_a = layout[0] == "T"
+    trans_b = layout[1] == "T"
+
+    a_shape = (m, k) if not trans_a else (k, m)
+    b_shape = (k, n) if not trans_b else (n, k)
+
+    a = torch.randn(a_shape, dtype=dtype, device=device, requires_grad=True)
+    b = torch.randn(b_shape, dtype=dtype, device=device, requires_grad=True)
+    a_ref = a.detach().clone().requires_grad_()
+    b_ref = b.detach().clone().requires_grad_()
+    torch.cuda.synchronize()
 
     # Ref
-    out_ref = x_ref @ w_ref
-    grad_out = torch.randn_like(out_ref)
-    out_ref.backward(grad_out)
-    x_grad_ref = x_ref.grad
-    w_grad_ref = w_ref.grad
+    a_mat = a_ref.T if trans_a else a_ref
+    b_mat = b_ref.T if trans_b else b_ref
+    c_ref = a_mat @ b_mat
+    grad_c = torch.randn_like(c_ref)
+    c_ref.backward(grad_c)
+    torch.cuda.synchronize()
 
     # Config + FWD + BWD
     config = Float8QuantConfig()
-    out = gemm_fp8_tensorwise(x, w, False, False, dtype, config)
-    out.backward(grad_out)
-    x_grad = x.grad
-    w_grad = w.grad
+    c = gemm_fp8_tensorwise(a, b, trans_a, trans_b, dtype, config)
+    c.backward(grad_c)
 
     # Check Shape
-    print("out shape: ", out.shape, out_ref.shape)
-    print("x_grad shape: ", x_grad.shape, x_grad_ref.shape)
-    print("w_grad shape: ", w_grad.shape, w_grad_ref.shape)
-    assert out.shape == out_ref.shape
-    assert x_grad.shape == x_grad_ref.shape
-    assert w_grad.shape == w_grad_ref.shape
+    # print("out shape: ", out.shape, out_ref.shape)
+    # print("x_grad shape: ", x_grad.shape, x_grad_ref.shape)
+    # print("w_grad shape: ", w_grad.shape, w_grad_ref.shape)
+    assert c.shape == c_ref.shape
+    assert a.grad.shape == a_ref.grad.shape
+    assert b.grad.shape == b_ref.grad.shape
 
     # Check Results
-    out_snr = compute_snr(out_ref, out)
-    print(f"Out-SNR: {out_snr:.2f} dB")
-    assert out_snr > 20, "out_snr too low"
+    c_snr = compute_snr(c_ref, c)
+    print(f"C-SNR: {c_snr:.2f} dB")
+    assert c_snr > 20, "c_snr too low"
 
-    xgrad_snr = compute_snr(x_grad_ref, x_grad)
-    print(f"XGrad-SNR: {xgrad_snr:.2f} dB")
-    assert xgrad_snr > 20, "xgrad_snr too low"
+    a_grad_snr = compute_snr(a_ref.grad, a.grad)
+    print(f"AGrad-SNR: {a_grad_snr:.2f} dB")
+    assert a_grad_snr > 20, "a_grad_snr too low"
 
-    wgrad_snr = compute_snr(w_grad_ref, w_grad)
-    print(f"WGrad-SNR: {wgrad_snr:.2f} dB")
-    assert wgrad_snr > 20, "wgrad_snr too low"
+    b_grad_snr = compute_snr(b_ref.grad, b.grad)
+    print(f"BGrad-SNR: {b_grad_snr:.2f} dB")
+    assert b_grad_snr > 20, "b_grad_snr too low"
