6d6797eb1922da51d578fd168288b743ae6434d3
Your Name <you@example.com>
2025-08-23 13:21:10 +0000

    update

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ def build_kernels_extension():
             Path(PROJECT_ROOT / "csrc"),
         ],
         sources=kernels_sources,
-        libraries=["hipblas"],
+        libraries=["hipblaslt"],
         **extra_flags,
     )
 
