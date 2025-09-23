17156657d54b081dedfc5b8679e63a16a255e236
kyle-256 <Kyle.Zhao@amd.com>
2025-09-23 08:06:06 +0000

    updating gemm fp8 kernel

---
+++ csrc/kernels/gemm/ck_gemm_kernel.h
@@ -0,0 +1,269 @@
+// Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+//
+// See LICENSE for license information.
+
+#pragma once
+
+#include "ck_tile/core.hpp"
+#include "ck_tile/host.hpp"
+#include "ck_tile/host/kernel_launch.hpp"
+#include "ck_tile/ops/elementwise/unary_element_wise_operation.hpp"
+#include "ck_tile/ops/epilogue.hpp"
+#include "ck_tile/ops/gemm.hpp"
+#include "ck_tile/ops/gemm_group_quant.hpp"
+#include <hip/hip_runtime.h>
+#include <string>
+
+#include "ck_gemm_kernel_config.h"
+#include "primus_turbo/arch.h"
+
+namespace primus_turbo {
+
+using RowMajor = ck_tile::tensor_layout::gemm::RowMajor;
+using ColMajor = ck_tile::tensor_layout::gemm::ColumnMajor;
+
+template <typename Kernel>
+inline void _launch_ck_gemm_kernel(const ck_tile::stream_config &stream_cfg,
+                                   QuantGemmKernelArgs &kargs, uint32_t num_cu) {
+    constexpr int kBlockPerCu = 1;
+    const dim3    blocks      = Kernel::BlockSize();
+    dim3          grids       = Kernel::MaxOccupancyGridSize(stream_cfg);
+    grids.x                   = std::min(grids.x, num_cu);
+    ck_tile::launch_kernel(stream_cfg, ck_tile::make_kernel<GemmConfig::kBlockPerCu>(
+                                           Kernel{}, grids, blocks, 0, kargs));
+}
+
+class CKGemmRunnerInterFace {
+public:
+    virtual ~CKGemmRunnerInterFace() = default;
+    // virtual void init_args() = 0;
+    virtual void run(const ck_tile::stream_config &stream_cfg, QuantGemmKernelArgs &kargs,
+                     uint32_t num_cu) = 0;
+};
+
+template <typename ADataType, typename BDataType, typename CDataType, typename ALayout,
+          typename BLayout, typename CLayout, typename TileConfig, typename AccDataType = float>
+class CKQuantGemmRunner : public CKGemmRunnerInterFace {
+public:
+    using GemmShape = ck_tile::TileGemmShape<
+        ck_tile::sequence<TileConfig::M_Tile, TileConfig::N_Tile, TileConfig::K_Tile>,
+        ck_tile::sequence<TileConfig::M_Warp, TileConfig::N_Warp, TileConfig::K_Warp>,
+        ck_tile::sequence<TileConfig::M_Warp_Tile, TileConfig::N_Warp_Tile,
+                          TileConfig::K_Warp_Tile>>;
+    static constexpr ck_tile::QuantType QuantMode = ck_tile::QuantType::RowColQuant;
+    using TilePartitioner                         = ck_tile::GemmTile1DPartitioner<GemmShape>;
+    using AQLayout                                = ck_tile::tensor_layout::gemm::RowMajor;
+    using BQLayout                                = ck_tile::tensor_layout::gemm::ColumnMajor;
+
+    using GemmUniversalTraits =
+        ck_tile::TileGemmQuantTraits<TileConfig::kPadM, TileConfig::kPadN, TileConfig::kPadK, false,
+                                     ALayout, BLayout, CLayout, QuantMode, AQLayout, BQLayout,
+                                     false, true>;
+
+    using GemmPipelineProblem = ck_tile::GemmPipelineProblemBase<
+        typename TypeConfig::ADataType, typename TypeConfig::BDataType,
+        typename TypeConfig::AccDataType, GemmShape, GemmUniversalTraits, ComputeDataType>;
+
+    using BaseGemmPipeline = ck_tile::BaseGemmPipelineAgBgCrCompV3<GemmPipelineProblem>;
+
+    const ck_tile::index_t    K_split      = 1;
+    const ck_tile::index_t    num_loop     = TilePartitioner::GetLoopNum(K_split);
+    const bool                has_hot_loop = BaseGemmPipeline::BlockHasHotloop(num_loop);
+    const ck_tile::TailNumber tail_num     = BaseGemmPipeline::GetBlockLoopTailNum(num_loop);
+
+    using QuantGemmProblem = ck_tile::GemmRowColQuantPipelineProblem<
+        ADataType, BDataType, AccDataType, AccDataType, GemmShape, GemmUniversalTraits, false, >;
+
+    // V3
+    using GemmPipeline = ck_tile::GemmPipelineAgBgCrCompV3<QuantGemmProblem>;
+
+    static constexpr ck_tile::memory_operation_enum MemoryOp = ck_tile::memory_operation_enum::set;
+    using GemmEpilogue = ck_tile::CShuffleEpilogue<ck_tile::CShuffleEpilogueProblem<
+        ADataType, BDataType, ck_tile::tuple<>, AccDataType, CDataType, ck_tile::tuple<>, CLayout,
+        ck_tile::element_wise::PassThrough, TilePartitioner::MPerBlock, TilePartitioner::NPerBlock,
+        TileConfig::M_Warp, TileConfig::N_Warp, TileConfig::M_Warp_Tile, TileConfig::N_Warp_Tile,
+        TileConfig::K_Warp_Tile, QuantGemmProblem::TransposeC, MemoryOp>>;
+
+    using Kernel = ck_tile::QuantGemmKernel<TilePartitioner, GemmPipeline, GemmEpilogue,
+                                            GemmUniversalTraits::kQuantType>;
+
+public:
+    void run(const ck_tile::stream_config &stream_cfg, QuantGemmKernelArgs &kargs,
+             uint32_t num_cu) override {
+        _launch_ck_gemm_kernel<Kernel>(stream_cfg, kargs, num_cu);
+    }
+};
+
+#ifdef PRIMUS_TURBO_GFX942
+template <typename ADataType, typename BDataType, typename CDataType, typename AccDataType,
+          typename ALayout, typename BLayout, typename CLayout>
+get_ck_gemm_instance_gfx942(const ck_tile::index_t m, const ck_tile::index_t n,
+                            const ck_tile::index_t k) {
+    std::unique_ptr<CKGemmRunnerInterFace> runner = nullptr;
+    if (get_current_arch() != GPUArch::GFX942) {
+        PRIMUS_TURBO_ERROR("Currently Arch != gfx942");
+    }
+
+    if constexpr (std::is_same_v<ADataType, ck_tile::bf8_t> ||
+                  std::is_same_v<ADataType, ck_tile::fp8_t>) {
+        if (n % 256 == 0) {
+            using TileConfig = CKGemmTileCfg_256x256x128_32x32x32_2x2x1;
+            using Runner     = CKQuantGemmRunner<GPUArch::GFX942, ADataType, BDataType, CDataType,
+                                                 ALayout, BLayout, CLayout, TileConfig, AccDataType>;
+            runner           = std::make_unique<Runner>();
+        } else if (n % 128 == 0) {
+            using TileConfig = CKGemmTileCfg_256x128x128_32x32x32_2x2x1;
+            using Runner     = CKQuantGemmRunner<GPUArch::GFX942, ADataType, BDataType, CDataType,
+                                                 ALayout, BLayout, CLayout, TileConfig, AccDataType>;
+            runner           = std::make_unique<Runner>();
+        } else {
+            using TileConfig = CKGemmTileCfg_256x128x128_32x32x32_2x2x1_padding;
+            using Runner     = CKQuantGemmRunner<GPUArch::GFX942, ADataType, BDataType, CDataType,
+                                                 ALayout, BLayout, CLayout, TileConfig, AccDataType>;
+            runner           = std::make_unique<Runner>();
+        }
+    } else {
+        PRIMUS_TURBO_ERROR("CK Gemm only support fp8/bf8");
+    }
+    return runner;
+}
+#endif
+
+#ifdef PRIMUS_TURBO_GFX950
+template <typename ADataType, typename BDataType, typename CDataType, typename AccDataType,
+          typename ALayout, typename BLayout, typename CLayout>
+get_ck_gemm_instance_gfx950(const ck_tile::index_t m, const ck_tile::index_t n,
+                            const ck_tile::index_t k) {
+    std::unique_ptr<CKGemmRunnerInterFace> runner = nullptr;
+    if (get_current_arch() != GPUArch::GFX950) {
+        PRIMUS_TURBO_ERROR("Currently Arch != gfx950");
+    }
+
+    if constexpr (std::is_same_v<ADataType, ck_tile::bf8_t> ||
+                  std::is_same_v<ADataType, ck_tile::fp8_t>) {
+        if (n % 256 == 0) {
+            using TileConfig = CKGemmTileCfg_256x256x128_16x16x128_2x2x1;
+            using Runner     = CKQuantGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType,
+                                                 ALayout, BLayout, CLayout, TileConfig, AccDataType>;
+            runner           = std::make_unique<Runner>();
+        } else {
+            using TileConfig = CKGemmTileCfg_128x128x128_32x32x64_2x2x1_padding;
+            using Runner     = CKQuantGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType,
+                                                 ALayout, BLayout, CLayout, TileConfig, AccDataType>;
+            runner           = std::make_unique<Runner>();
+        }
+    } else {
+        PRIMUS_TURBO_ERROR("CK Gemm only support fp8/bf8");
+    }
+    return runner;
+}
+#endif
+
+template <typename ADataType, typename BDataType, typename CDataType, typename AccDataType,
+          typename ALayout, typename BLayout, typename CLayout>
+std::unique_ptr<CKGemmRunnerInterFace>
+get_ck_gemm_instance(const ck_tile::index_t m, const ck_tile::index_t n, const ck_tile::index_t k) {
+    const GPUArch arch = get_current_arch();
+    switch (arch) {
+#ifdef PRIMUS_TURBO_GFX942
+    case GPUArch::GFX942: {
+        return get_ck_gemm_instance_gfx942<ADataType, BDataType, CDataType, AccDataType, ALayout,
+                                           BLayout, CLayout>(m, n, k);
+    }
+#endif
+#ifdef PRIMUS_TURBO_GFX950
+    case GPUArch::GFX950: {
+        return get_ck_gemm_instance_gfx950<ADataType, BDataType, CDataType, AccDataType, ALayout,
+                                           BLayout, CLayout>(m, n, k);
+    }
+#endif
+    default:
+        PRIMUS_TURBO_ERROR("Unsupported arch in get_ck_gemm_instance()");
+    }
+}
+// clang-format off
+// **************** GFX942 Instantiation ****************
+#ifdef PRIMUS_TURBO_GFX942
+#define DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(AType, BType, CType, ALayout, BLayout, CLayout)           \
+extern template std::unique_ptr<CKGemmRunnerInterFace>                                       \
+get_ck_gemm_instance_gfx942<AType, BType, CType, float, ALayout, BLayout, CLayout>(         \
+                            const ck_tile::index_t, const ck_tile::index_t, const ck_tile::index_t);
+
+// FP8_E4M3 * FP8_E4M3 = FP16
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E4M3 * FP8_E4M3 = BF16
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E5M2 * FP8_E5M2 = FP16
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E5M2 * FP8_E5M2 = BF16
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX942_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, ColMajor, RowMajor, RowMajor)
+
+#undef DECL_CK_GEMM_GFX942_EXTERN_INSTANCE
+
+#define DECL_CK_GEMM_GFX942_INSTANCE(AType, BType, CType, ALayout, BLayout, CLayout)             \
+template std::unique_ptr<CKGemmRunnerInterFace>                                                  \
+get_ck_gemm_instance_gfx942<AType, BType, CType, float, ALayout, BLayout, CLayout>(              \
+                            const ck_tile::index_t, const ck_tile::index_t, const ck_tile::index_t);
+#endif
+// ******************************************************
+
+
+// **************** GFX950 Instantiation ****************
+#ifdef PRIMUS_TURBO_GFX950
+#define DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(AType, BType, CType, ALayout, BLayout, CLayout)       \
+extern template std::unique_ptr<CKGemmRunnerInterFace>                                     \
+get_ck_gemm_instance_gfx950<AType, BType, CType, float, ALayout, BLayout, CLayout>(               \
+                            const ck_tile::index_t, const ck_tile::index_t, const ck_tile::index_t);
+
+// FP16 * FP16 = FP16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::half_t, ck_tile::half_t, ck_tile::half_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::half_t, ck_tile::half_t, ck_tile::half_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::half_t, ck_tile::half_t, ck_tile::half_t, ColMajor, RowMajor, RowMajor)
+
+// BF16 * BF16 = BF16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bfloat16_t, ck_tile::bfloat16_t, ck_tile::bfloat16_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bfloat16_t, ck_tile::bfloat16_t, ck_tile::bfloat16_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bfloat16_t, ck_tile::bfloat16_t, ck_tile::bfloat16_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E4M3 * FP8_E4M3 = FP16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::half_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E4M3 * FP8_E4M3 = BF16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::fp8_t, ck_tile::fp8_t, ck_tile::bfloat16_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E5M2 * FP8_E5M2 = FP16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::half_t, ColMajor, RowMajor, RowMajor)
+
+// FP8_E5M2 * FP8_E5M2 = BF16
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, RowMajor, ColMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, RowMajor, RowMajor, RowMajor)
+DECL_CK_GEMM_GFX950_EXTERN_INSTANCE(ck_tile::bf8_t, ck_tile::bf8_t, ck_tile::bfloat16_t, ColMajor, RowMajor, RowMajor)
+
+#undef DECL_CK_GEMM_GFX950_EXTERN_INSTANCE
+
+#define DECL_CK_GEMM_GFX950_INSTANCE(AType, BType, CType, ALayout, BLayout, CLayout)                \
+template std::unique_ptr<CKGemmRunnerInterFace>                                                     \
+get_ck_gemm_instance_gfx950<AType, BType, CType, float, ALayout, BLayout, CLayout>(         \
+                                    const ck_tile::index_t, const ck_tile::index_t, const ck_tile::index_t);
+#endif
+// ******************************************************
+
+// clang-format on
+} // namespace primus_turbo
--- csrc/pytorch/gemm/ck_gemm.cpp
+++ csrc/pytorch/gemm/ck_gemm.cpp
@@ -2,8 +2,90 @@
 //
 // See LICENSE for license information.
 
+#include "../extensions.h"
+#include "../type_traits.h"
+#include "primus_turbo/arch.h"
+#include "primus_turbo/grouped_gemm.h"
+
 namespace primus_turbo::pytorch {
 
-// TODO: CK
+at::Tensor grouped_gemm_fp8(at::Tensor &a, at::Tensor &b, at::Tensor &a_scales,
+                            at::Tensor &b_scales, const bool transA, const bool transB,
+                            at::ScalarType out_dtype, const std::string &granularity,
+                            c10::optional<int64_t> num_cu) {
+
+    // Check
+    PRIMUS_TURBO_CHECK(is_8bit_floating_point_dtype(a.scalar_type()));
+    PRIMUS_TURBO_CHECK(is_8bit_floating_point_dtype(b.scalar_type()));
+    PRIMUS_TURBO_CHECK(a.scalar_type() == b.scalar_type(), "a and b dtype mismatch");
+    PRIMUS_TURBO_CHECK(out_dtype == at::kBFloat16 || out_dtype == at::kHalf,
+                       "out_dtype must be kBFloat16 or kHalf");
+
+    // Determine output tensor size based on transA and transB
+    const int64_t bs = b.size(0);
+    const int64_t m  = transA ? a.size(1) : a.size(0);
+    const int64_t n  = transB ? b.size(1) : b.size(2);
+    const int64_t k  = transA ? a.size(0) : a.size(1);
+
+    // Process Scale
+    at::Tensor aq_tensor;
+    at::Tensor bq_tensor;
+    if (granularity == "TENSORWISE") {
+        aq_tensor = a_scales.reshape({1, 1}).expand({m, 1});
+        bq_tensor = b_scales.reshape({1, 1, 1}).expand({bs, 1, n});
+    } else {
+        aq_tensor = a_scales.clone();
+        bq_tensor = b_scales.clone();
+    }
+    aq_tensor = aq_tensor.contiguous();
+    bq_tensor = bq_tensor.contiguous();
+
+    at::Tensor c      = at::empty({m, n}, at::dtype(out_dtype).device(at::kCUDA));
+    auto       stream = at::cuda::getCurrentCUDAStream();
+
+    if (a.dtype() == at::kFloat8_e4m3fnuz || a.dtype() == at::kFloat8_e4m3fn) {
+        using AType = typename TorchToCKTileType<at::kFloat8_e4m3fnuz>::type;
+        using BType = AType;
+
+        if (out_dtype == at::kBFloat16) {
+            using CType = typename TorchToCKTileType<at::kBFloat16>::type;
+            auto params = make_ck_groued_gemm_fp8_params<AType, BType, CType, float>(
+                args_tensor.data_ptr(), a, b, c, aq_tensor, bq_tensor, group_lens, group_offs,
+                transA, transB, bs, m, n, k, stream, get_grouped_gemm_num_cu(num_cu));
+            ck_grouped_gemm_fp8<AType, BType, CType, float>(params);
+        } else if (out_dtype == at::kHalf) {
+            using CType = typename TorchToCKTileType<at::kHalf>::type;
+            auto params = make_ck_groued_gemm_fp8_params<AType, BType, CType, float>(
+                args_tensor.data_ptr(), a, b, c, aq_tensor, bq_tensor, group_lens, group_offs,
+                transA, transB, bs, m, n, k, stream, get_grouped_gemm_num_cu(num_cu));
+            ck_grouped_gemm_fp8<AType, BType, CType, float>(params);
+        } else {
+            PRIMUS_TURBO_CHECK(false, "Unsupported out_dtype for fp8 e4m3");
+        }
+    } else if (a.dtype() == at::kFloat8_e5m2fnuz || a.dtype() == at::kFloat8_e5m2) {
+        using AType = typename TorchToCKTileType<at::kFloat8_e5m2fnuz>::type;
+        using BType = AType;
+
+        if (out_dtype == at::kBFloat16) {
+            using CType = typename TorchToCKTileType<at::kBFloat16>::type;
+            auto params = make_ck_groued_gemm_fp8_params<AType, BType, CType, float>(
+                args_tensor.data_ptr(), a, b, c, aq_tensor, bq_tensor, group_lens, group_offs,
+                transA, transB, bs, m, n, k, stream, get_grouped_gemm_num_cu(num_cu));
+            ck_grouped_gemm_fp8<AType, BType, CType, float>(params);
+        } else if (out_dtype == at::kHalf) {
+            using CType = typename TorchToCKTileType<at::kHalf>::type;
+            auto params = make_ck_groued_gemm_fp8_params<AType, BType, CType, float>(
+                args_tensor.data_ptr(), a, b, c, aq_tensor, bq_tensor, group_lens, group_offs,
+                transA, transB, bs, m, n, k, stream, get_grouped_gemm_num_cu(num_cu));
+            ck_grouped_gemm_fp8<AType, BType, CType, float>(params);
+        } else {
+            PRIMUS_TURBO_CHECK(false, "Unsupported out_dtype for fp8 e5m2");
+        }
+    } else {
+        PRIMUS_TURBO_CHECK(false, "GroupedGemmFp8 only support fp8/bf8");
+    }
+
+    return c;
+}
 
 } // namespace primus_turbo::pytorch
