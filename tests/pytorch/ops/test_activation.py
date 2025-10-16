e752a308535647405a3b4c7321deeba4bf867ada
Ruibin.Cheung <ruibzhan@amd.com>
2025-10-16 03:26:02 +0000

    refine ut

--- tests/pytorch/ops/test_activation.py
+++ tests/pytorch/ops/test_activation.py
@@ -10,7 +10,7 @@ import pytest
 import torch
 import torch.nn.functional as F
 
-from primus_turbo.pytorch.ops.activation import swiglu_with_probs
+from primus_turbo.pytorch.ops.activation import geglu_with_probs, swiglu_with_probs
 from tests.test_utils import get_tolerances
 
 torch.manual_seed(42)
@@ -42,6 +42,15 @@ def generate_tokens_per_expert_list(num_experts: int, num_tokens: int):
     return parts
 
 
+# NOTE: Align precision with torch.compile
+@torch.compile
+def geglu_with_probs_ref(x: torch.Tensor, probs: torch.Tensor):
+    dtype = x.dtype
+    x = torch.chunk(x, 2, dim=-1)
+    res = F.gelu(x[0]) * x[1]
+    return (res * probs).to(dtype)
+
+
 @pytest.mark.parametrize(
     "batch_size",
     [1, 8],
@@ -66,14 +75,21 @@ def generate_tokens_per_expert_list(num_experts: int, num_tokens: int):
 )
 @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
 @pytest.mark.parametrize("with_tokens_per_expert", [False, True])
-def test_swiglu_with_probs(batch_size, sequence_length, hidden_size, dtype, with_tokens_per_expert):
+@pytest.mark.parametrize("act_type", ["swiglu", "geglu"])
+def test_glu_with_probs(batch_size, sequence_length, hidden_size, dtype, with_tokens_per_expert, act_type):
     if not torch.cuda.is_available():
         pytest.skip("CUDA not available")
+
+    if act_type == "swiglu":
+        func = swiglu_with_probs
+        ref_func = swiglu_with_probs_ref
+    elif act_type == "geglu":
+        func = geglu_with_probs
+        ref_func = geglu_with_probs_ref
 
     device = "cuda"
 
     probs_dtype = torch.float32
-
     x = torch.randn(
         batch_size, sequence_length, hidden_size * 2, device=device, dtype=dtype, requires_grad=True
     )
@@ -96,8 +112,8 @@ def test_swiglu_with_probs(batch_size, sequence_length, hidden_size, dtype, with
     else:
         row_mask = None
 
-    out = swiglu_with_probs(x, probs, row_mask)
-    out_ref = swiglu_with_probs_ref(x_ref, probs_ref)
+    out = func(x, probs, row_mask)
+    out_ref = ref_func(x_ref, probs_ref)
     torch.testing.assert_close(out, out_ref, **get_tolerances(dtype))
 
     out.backward(torch.ones_like(out))
@@ -110,74 +126,3 @@ def test_swiglu_with_probs(batch_size, sequence_length, hidden_size, dtype, with
 
     torch.testing.assert_close(grad_x, grad_x_ref.view_as(grad_x), **get_tolerances(dtype))
     torch.testing.assert_close(grad_probs, grad_probs_ref.view_as(grad_probs), **get_tolerances(probs_dtype))
-
-
-# # NOTE: Align precision with torch.compile
-# @torch.compile
-# def geglu_with_probs_ref(x: torch.Tensor, probs: torch.Tensor):
-#     dtype = x.dtype
-#     x = torch.chunk(x, 2, dim=-1)
-#     res = F.gelu(x[0]) * x[1]
-#     return (res * probs).to(dtype)
-
-
-# @pytest.mark.parametrize(
-#     "num_tokens",
-#     [
-#         1,
-#         128,
-#         2048,
-#         2025,
-#         8192 * 8,
-#     ],
-# )
-# @pytest.mark.parametrize(
-#     "hidden_size",
-#     [
-#         128,
-#         256,
-#         2048,
-#     ],
-# )
-# @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
-# @pytest.mark.parametrize("with_tokens_per_expert", [False, True])
-# def test_geglu_with_probs(num_tokens, hidden_size, dtype, with_tokens_per_expert):
-#     if not torch.cuda.is_available():
-#         pytest.skip("CUDA not available")
-
-#     device = "cuda"
-
-#     probs_dtype = torch.float32
-
-#     x = torch.randn(num_tokens, hidden_size * 2, device=device, dtype=dtype, requires_grad=True)
-#     probs = torch.rand(num_tokens, device=device, dtype=probs_dtype, requires_grad=True)
-
-#     x_ref = x.clone().detach()
-#     x_ref.requires_grad_()
-#     probs_ref = probs.clone().detach()
-#     probs_ref.requires_grad_()
-
-#     if with_tokens_per_expert:
-#         num_experts = 64
-#         tokens_per_expert = torch.tensor(
-#             generate_tokens_per_expert_list(num_experts, num_tokens), device=device, requires_grad=False
-#         )
-#         row_mask = torch.zeros(num_tokens, device=device, dtype=torch.int64, requires_grad=False)
-#         row_mask[: torch.sum(tokens_per_expert)] = 1
-#     else:
-#         row_mask = None
-
-#     out = geglu_with_probs(x, probs, row_mask)
-#     out_ref = geglu_with_probs_ref(x_ref, probs_ref.unsqueeze(-1))
-#     torch.testing.assert_close(out, out_ref, **get_tolerances(dtype))
-
-#     out.backward(torch.ones_like(out))
-#     grad_x = x.grad.clone()
-#     grad_probs = probs.grad.clone()
-
-#     out_ref.backward(torch.ones_like(out_ref))
-#     grad_x_ref = x_ref.grad.clone()
-#     grad_probs_ref = probs_ref.grad.clone()
-
-#     torch.testing.assert_close(grad_x, grad_x_ref, **get_tolerances(dtype))
-#     torch.testing.assert_close(grad_probs, grad_probs_ref, **get_tolerances(probs_dtype))
