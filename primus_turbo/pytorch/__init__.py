1773df323931efe29d4a0b1320468b6803fdfdb4
xiaobochen-amd <xiaobo.chen@amd.com>
2025-11-14 07:30:54 +0000

    rename float8.py to low_precision.py

--- primus_turbo/pytorch/__init__.py
+++ primus_turbo/pytorch/__init__.py
@@ -4,7 +4,7 @@ import primus_turbo.pytorch._C
 import primus_turbo.pytorch.deep_ep
 import primus_turbo.pytorch.modules
 import primus_turbo.pytorch.ops
-from primus_turbo.pytorch.core import float8
+from primus_turbo.pytorch.core import low_precision
 
-float8_e4m3 = float8.float8_e4m3
-float8_e5m2 = float8.float8_e5m2
+float8_e4m3 = low_precision.float8_e4m3
+float8_e5m2 = low_precision.float8_e5m2
