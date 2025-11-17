b808c21b339dfd11700579545159027b9bdb6838
xiaobochen-amd <xiaobo.chen@amd.com>
2025-11-17 01:57:40 +0000

    add float8_e8m0 & float4x2_e2m1 dtype

--- csrc/include/primus_turbo/dtype.h
+++ csrc/include/primus_turbo/dtype.h
@@ -5,6 +5,7 @@
 #pragma once
 #include <cstdint>
 
+#include "primus_turbo/float4.h"
 #include "primus_turbo/float8.h"
 #include <hip/hip_bfloat16.h>
 #include <hip/hip_fp16.h>
@@ -14,16 +15,14 @@ namespace primus_turbo {
 
 namespace dtype {
 
-using float64     = double;
-using float32     = float;
-using float16     = half;
-using bfloat16    = hip_bfloat16;
-using float8_e4m3 = float8_e4m3_t;
-using float8_e5m2 = float8_e5m2_t;
-// using float8_e4m3 = __hip_fp8_e4m3_fnuz;
-// using float8_e5m2 = __hip_fp8_e5m2_fnuz;
-// using float8_e4m3 = __hip_fp8_e4m3;
-// using float8_e5m2 = __hip_fp8_e5m2;
+using float64       = double;
+using float32       = float;
+using float16       = half;
+using bfloat16      = hip_bfloat16;
+using float8_e4m3   = float8_e4m3_t;
+using float8_e5m2   = float8_e5m2_t;
+using float8_e8m0   = float8_e8m0_t;
+using float4x2_e2m1 = float4x2_e2m1_t;
 
 using int64 = int64_t;
 using int32 = int32_t;
