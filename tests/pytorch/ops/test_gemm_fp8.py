cfae3ffcd181c6e90ce12fc5cfdd80395440516a
RuibinCheung <ruibzhan@amd.com>
2025-11-20 07:15:20 +0000

    feat(mxfp8): add k padding in bwd

--- tests/pytorch/ops/test_gemm_fp8.py
+++ tests/pytorch/ops/test_gemm_fp8.py
@@ -142,13 +142,16 @@ def test_gemm_fp8_blockwise(m, n, k, layout, format, dtype, granularity, block_s
 
 
 @pytest.mark.parametrize("m", [256, 512, 1024])
-@pytest.mark.parametrize("n", [256, 2048, 4096])
-@pytest.mark.parametrize("k", [128, 512, 1024, 2048])
+@pytest.mark.parametrize("n", [256, 352, 1024, 2048])
+@pytest.mark.parametrize("k", [128, 127, 512, 1024])
 @pytest.mark.parametrize("layout", ["NT"])
 @pytest.mark.parametrize("format", [Format.E4M3, Format.E5M2, Format.HYBRID])
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16])
 @pytest.mark.parametrize("granularity", [ScalingGranularity.MX_BLOCKWISE])
 def test_gemm_mxfp8(m, n, k, layout, format, dtype, granularity):
+    # NOTE: user need to ensure m and n are multiples of 16.
+    assert m % 16 == 0 and n % 16 == 0, "Assue m and n are multiples of 16."
+
     from primus_turbo.pytorch.core.float8 import check_mxfp8_support
 
     # Skip unit test on gfx942.
