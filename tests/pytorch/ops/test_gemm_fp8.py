8d74c2478f282b5ecab4cf219f04054280cf111d
RuibinCheung <ruibzhan@amd.com>
2025-12-03 07:14:08 +0000

    udpate script

--- tests/pytorch/ops/test_gemm_fp8.py
+++ tests/pytorch/ops/test_gemm_fp8.py
@@ -141,14 +141,21 @@ def test_gemm_fp8_blockwise(m, n, k, layout, format, dtype, granularity, block_s
     assert b_grad_snr > snr_threshold, "b_grad_snr too low"
 
 
-@pytest.mark.parametrize("m", [256, 512, 1024])
-@pytest.mark.parametrize("n", [256, 352, 1024, 2048])
-@pytest.mark.parametrize("k", [128, 160, 512, 1024])
+@pytest.mark.parametrize(
+    "problem_size",
+    [
+        # [4096, 576, 5120],
+        [16384, 576, 7168],
+    ],
+)
 @pytest.mark.parametrize("layout", ["NT"])
-@pytest.mark.parametrize("format", [Format.E4M3, Format.E5M2, Format.HYBRID])
-@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16])
+@pytest.mark.parametrize("format", [Format.E4M3])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
 @pytest.mark.parametrize("granularity", [ScalingGranularity.MX_BLOCKWISE])
-def test_gemm_mxfp8(m, n, k, layout, format, dtype, granularity):
+def test_gemm_mxfp8_save(problem_size, layout, format, dtype, granularity):
+    torch.manual_seed(42)
+
+    m,n,k = problem_size
     # NOTE: user need to ensure m, n and k are multiples of 16.
     assert m % 16 == 0 and n % 16 == 0 and k % 16 == 0, "Assume m, n and k are multiples of 16."
 
@@ -171,39 +178,79 @@ def test_gemm_mxfp8(m, n, k, layout, format, dtype, granularity):
 
     a = torch.randn(a_shape, dtype=dtype, device=device, requires_grad=True)
     b = torch.randn(b_shape, dtype=dtype, device=device, requires_grad=True)
-    a_ref = a.detach().clone().requires_grad_()
-    b_ref = b.detach().clone().requires_grad_()
-    torch.cuda.synchronize()
-
-    # Ref
-    a_mat = a_ref.T if trans_a else a_ref
-    b_mat = b_ref.T if trans_b else b_ref
-    c_ref = a_mat @ b_mat
-    grad_c = torch.randn_like(c_ref)
-    c_ref.backward(grad_c)
     torch.cuda.synchronize()
 
     # Config + FWD + BWD
     config = Float8QuantConfig(granularity=granularity, format=format, block_size=32)
     print(config)
     c = gemm_fp8(a, b, trans_a, trans_b, dtype, config)
+    c.backward(torch.ones_like(c))
+
+    torch.save(c, "c.pt")
+    torch.save(a.grad, "grad_a.pt")
+    torch.save(b.grad, "grad_b.pt")
+
+
+@pytest.mark.parametrize(
+    "problem_size",
+    [
+        # [4096, 576, 5120],
+        [16384, 576, 7168],
+    ],
+)
+@pytest.mark.parametrize("layout", ["NT"])
+@pytest.mark.parametrize("format", [Format.E4M3])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+@pytest.mark.parametrize("granularity", [ScalingGranularity.MX_BLOCKWISE])
+def test_gemm_mxfp8_compare(problem_size, layout, format, dtype, granularity):
+    torch.manual_seed(42)
+
+    m,n,k = problem_size
+    # NOTE: user need to ensure m, n and k are multiples of 16.
+    assert m % 16 == 0 and n % 16 == 0 and k % 16 == 0, "Assume m, n and k are multiples of 16."
+
+    from primus_turbo.pytorch.core.low_precision import check_mxfp8_support
+
+    # Skip unit test on gfx942.
+    mxfp8_supported, reason = check_mxfp8_support()
+    if not mxfp8_supported:
+        pytest.skip(reason)
+
+    print(f"\nM={m}, N={n}, K={k}, layout={layout}, dtype={dtype}, format={format}")
+
+    device = "cuda:0"
+
+    trans_a = layout[0] == "T"
+    trans_b = layout[1] == "T"
+
+    a_shape = (m, k) if not trans_a else (k, m)
+    b_shape = (k, n) if not trans_b else (n, k)
+
+    a = torch.randn(a_shape, dtype=dtype, device=device, requires_grad=True)
+    b = torch.randn(b_shape, dtype=dtype, device=device, requires_grad=True)
+    torch.cuda.synchronize()
+
+    # Ref
+    a_mat = a.T if trans_a else a
+    b_mat = b.T if trans_b else b
+    c = a_mat @ b_mat
+    grad_c = torch.ones_like(c)
     c.backward(grad_c)
 
-    # Check Shape
-    assert c.shape == c_ref.shape
-    assert a.grad.shape == a_ref.grad.shape
-    assert b.grad.shape == b_ref.grad.shape
+    a_grad_ref = torch.load("grad_a.pt")
+    b_grad_ref = torch.load("grad_b.pt")
+    c_ref = torch.load("c.pt")
 
     snr_threshold = 25 if format == Format.E4M3 else 20
-    # Check Results
-    c_snr = compute_snr(c_ref, c)
+
+    c_snr = compute_snr(c, c_ref)
     print(f"C-SNR: {c_snr:.2f} dB")
     assert c_snr > snr_threshold, "c_snr too low"
 
-    a_grad_snr = compute_snr(a_ref.grad, a.grad)
+    a_grad_snr = compute_snr(a.grad, a_grad_ref)
     print(f"AGrad-SNR: {a_grad_snr:.2f} dB")
     assert a_grad_snr > snr_threshold, "a_grad_snr too low"
 
-    b_grad_snr = compute_snr(b_ref.grad, b.grad)
+    b_grad_snr = compute_snr(b.grad, b_grad_ref)
     print(f"BGrad-SNR: {b_grad_snr:.2f} dB")
     assert b_grad_snr > snr_threshold, "b_grad_snr too low"
