0ec53f1f469d1328c6c36c2d352106887fc448bf
RuibinCheung <ruibzhan@amd.com>
2025-12-11 08:32:48 +0000

    Add mxfp4 benchmark

--- tests/pytorch/ops/test_gemm_fp4.py
+++ tests/pytorch/ops/test_gemm_fp4.py
@@ -16,14 +16,46 @@ from primus_turbo.pytorch.core.low_precision import (
     ScalingGranularity,
 )
 from primus_turbo.pytorch.ops.gemm_fp4 import gemm_fp4
-from tests.pytorch.test_utils import compute_snr
 
 torch.manual_seed(42)
 
+PROBLEM_SIZES = [
+    [16384, 22016, 4096],
+    [16384, 4096, 22016],
+    [22016, 4096, 16384],
+    [16384, 12288, 4096],
+    [16384, 4096, 12288],
+    [16384, 4096, 11008],
+    [16384, 11008, 4096],
+    [12288, 4096, 16384],
+    [4096, 11008, 16384],
+    [16384, 4096, 4096],
+    [4096, 4096, 16384],
+    [28672, 57344, 8192],
+    [28672, 8192, 57344],
+    [57344, 8192, 28672],
+    [28672, 8192, 28672],
+    [28672, 28672, 8192],
+    [28672, 8192, 8192],
+    [8192, 28672, 28672],
+    [28672, 10240, 8192],
+    [28672, 8192, 10240],
+    [10240, 8192, 28672],
+    [8192, 8192, 28672],
+    [16384, 28672, 4096],
+    [28672, 4096, 16384],
+    [16384, 4096, 28672],
+    [16384, 14336, 4096],
+    [16384, 4096, 14336],
+    [4096, 14336, 16384],
+    [16384, 4096, 4096],
+    [16384, 6144, 4096],
+    [16384, 4096, 6144],
+    [6144, 4096, 16384],
+    [4096, 4096, 16384],
+]
 
-@pytest.mark.parametrize("m", [256, 512, 1024])
-@pytest.mark.parametrize("n", [256, 352, 1024, 2048])
-@pytest.mark.parametrize("k", [128, 160, 512, 1024])
+
 @pytest.mark.parametrize("layout", ["NT"])
 @pytest.mark.parametrize(
     "format",
@@ -35,20 +67,11 @@ torch.manual_seed(42)
     "dtype",
     [
         torch.bfloat16,
-        torch.float16,
     ],
 )
 @pytest.mark.parametrize("granularity", [ScalingGranularity.MX_BLOCKWISE])
-@pytest.mark.parametrize("backend", [None, BackendType.HIPBLASLT])
-@pytest.mark.parametrize("auto_tune", [False, True])
-def test_gemm_mxfp4(m, n, k, layout, format, dtype, granularity, backend, auto_tune):
-    # Skip redundant test: auto_tune is ignored when backend is explicitly specified
-    if backend is not None and auto_tune:
-        pytest.skip("auto_tune is ignored when backend is explicitly specified")
-
-    # NOTE: user need to ensure m, n and k are multiples of 16.
-    assert m % 16 == 0 and n % 16 == 0 and k % 16 == 0, "Assume m, n and k are multiples of 16."
-
+@pytest.mark.parametrize("backend", [BackendType.HIPBLASLT])
+def test_gemm_mxfp4(layout, format, dtype, granularity, backend):
     from primus_turbo.pytorch.core.low_precision import check_mxfp4_support
 
     # Skip unit test on gfx942.
@@ -58,17 +81,13 @@ def test_gemm_mxfp4(m, n, k, layout, format, dtype, granularity, backend, auto_t
 
     # Set backend and auto_tune config
     GlobalBackendManager.set_gemm_backend(backend)
-    GlobalBackendManager.set_auto_tune(auto_tune)
-
-    print(
-        f"\nM={m}, N={n}, K={k}, layout={layout}, dtype={dtype}, format={format}, "
-        f"backend={backend}, auto_tune={auto_tune}"
-    )
 
     device = "cuda:0"
 
     trans_a = layout[0] == "T"
     trans_b = layout[1] == "T"
+
+    m, n, k = PROBLEM_SIZES[0]
 
     a_shape = (m, k) if not trans_a else (k, m)
     b_shape = (k, n) if not trans_b else (n, k)
@@ -76,59 +95,48 @@ def test_gemm_mxfp4(m, n, k, layout, format, dtype, granularity, backend, auto_t
     a = torch.randn(a_shape, dtype=dtype, device=device, requires_grad=True)
     b = torch.randn(b_shape, dtype=dtype, device=device, requires_grad=True)
 
-    a_ref = a.detach().clone().requires_grad_()
-    b_ref = b.detach().clone().requires_grad_()
-    torch.cuda.synchronize()
-
-    # Ref
-    a_mat = a_ref.T if trans_a else a_ref
-    b_mat = b_ref.T if trans_b else b_ref
-    c_ref = a_mat @ b_mat
-    c_ref.backward(torch.ones_like(c_ref))
-    torch.cuda.synchronize()
-
-    # Config + FWD + BWD
-    # NOTE: scaling recipe reference: https://arxiv.org/pdf/2509.25149
     config = Float4QuantConfig(
         granularity=granularity, format=format, block_size=32, scale_dtype=ScaleDtype.E8M0
     )
-    config.scaling_recipe["a_fwd"] = Float4ScalingRecipe(
-        use_2d_block=False, use_hadamard_transform=False, use_sr=False
-    )
-    config.scaling_recipe["b_fwd"] = Float4ScalingRecipe(
-        use_2d_block=True, use_hadamard_transform=False, use_sr=False
-    )
-    config.scaling_recipe["grad_bwd"] = Float4ScalingRecipe(
-        use_2d_block=False, use_hadamard_transform=True, use_sr=True
-    )
-    config.scaling_recipe["a_bwd"] = Float4ScalingRecipe(
-        use_2d_block=False, use_hadamard_transform=False, use_sr=False
-    )
-    config.scaling_recipe["b_bwd"] = Float4ScalingRecipe(
-        use_2d_block=True, use_hadamard_transform=True, use_sr=True
-    )
-    print(config)
-    c = gemm_fp4(a, b, trans_a, trans_b, dtype, config)
-    c.backward(torch.ones_like(c))
+    config.scaling_recipe["a_fwd"] = Float4ScalingRecipe(use_2d_block=False, use_sr=False)
+    config.scaling_recipe["b_fwd"] = Float4ScalingRecipe(use_2d_block=True, use_sr=False)
+    config.scaling_recipe["grad_bwd"] = Float4ScalingRecipe(use_2d_block=False, use_sr=True)
+    config.scaling_recipe["a_bwd"] = Float4ScalingRecipe(use_2d_block=False, use_sr=False)
+    config.scaling_recipe["b_bwd"] = Float4ScalingRecipe(use_2d_block=True, use_sr=True)
 
-    # Check Shape
-    assert c.shape == c_ref.shape
-    assert a.grad.shape == a_ref.grad.shape
-    assert b.grad.shape == b_ref.grad.shape
+    for _ in range(0, 10):
+        c = gemm_fp4(a, b, trans_a, trans_b, dtype, config)
+        c.backward(torch.ones_like(c))
 
-    snr_threshold = 10
-    # Check Results
-    c_snr = compute_snr(c_ref, c)
-    print(f"C-SNR: {c_snr:.2f} dB")
-    assert c_snr > snr_threshold, "c_snr too low"
+    torch.cuda.synchronize()
 
-    a_grad_snr = compute_snr(a_ref.grad, a.grad)
-    print(f"AGrad-SNR: {a_grad_snr:.2f} dB")
-    assert a_grad_snr > snr_threshold, "a_grad_snr too low"
+    prof = torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("./profile"),
+        record_shapes=True,
+        profile_memory=False,
+        with_stack=True,
+    )
 
-    b_grad_snr = compute_snr(b_ref.grad, b.grad)
-    print(f"BGrad-SNR: {b_grad_snr:.2f} dB")
-    assert b_grad_snr > snr_threshold, "b_grad_snr too low"
+    prof.start()
+    for _ in range(0, 10):
+        for problem_size in PROBLEM_SIZES:
+            m, n, k = problem_size
+            a_shape = (m, k) if not trans_a else (k, m)
+            b_shape = (k, n) if not trans_b else (n, k)
+
+            a = torch.randn(a_shape, dtype=dtype, device=device, requires_grad=True)
+            b = torch.randn(b_shape, dtype=dtype, device=device, requires_grad=True)
+
+            a.detach().clone().requires_grad_()
+            b.detach().clone().requires_grad_()
+
+            c = gemm_fp4(a, b, trans_a, trans_b, dtype, config)
+            c.backward(torch.ones_like(c))
+        prof.step()
+    torch.cuda.synchronize()
+
+    prof.stop()
 
     # Reset config and caches
     GlobalBackendManager.reset()
