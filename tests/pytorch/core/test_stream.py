4354df3333ff56744c5889334cb61e176b0ca0d3
xiaobochen-amd <xiaobo.chen@amd.com>
2025-12-11 18:58:11 +0800

    feat: add multi-process tests & clean unused code. (#176)

--- tests/pytorch/core/test_stream.py
+++ tests/pytorch/core/test_stream.py
@@ -4,7 +4,7 @@ import torch
 from primus_turbo.pytorch.core import TurboStream
 
 
-@pytest.mark.parametrize("device", [1, "cuda", "cuda:2", torch.device("cuda:0")])
+@pytest.mark.parametrize("device", [0, "cuda", "cuda:0", torch.device("cuda:0")])
 @pytest.mark.parametrize("cu_masks", [None, [0xFFFFFFFF], [0xFFFFFFFF, 0xFFFFFFFF]])
 def test_turbo_stream(device, cu_masks):
     turbo_stream = TurboStream(device=device, cu_masks=cu_masks)
