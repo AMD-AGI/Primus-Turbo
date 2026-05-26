ea8a95c58f89400a093386e2f807ce73b2bbf099
liying <liyingli@amd.com>
2026-05-26 09:40:21 +0000

    fix find_free_port by review

--- tests/jax/test_utils.py
+++ tests/jax/test_utils.py
@@ -132,7 +132,6 @@ def _find_free_port():
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
 
