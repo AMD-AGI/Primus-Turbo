a7e50e7700eccf7e16e7ca93da03754a7056e6df
RuibinCheung <ruibzhan@amd.com>
2026-06-30 08:26:18 +0000

    feat: add quantized tensor support for mxfp8 grouped gemm

--- csrc/pytorch/bindings_pytorch.cpp
+++ csrc/pytorch/bindings_pytorch.cpp
@@ -55,6 +55,10 @@ TORCH_LIBRARY(primus_turbo_cpp_extension, m) {
           "bool shuffle_scale=False, bool shuffle_out=False) -> Tensor[]");
     m.def("dequantize_mxfp4(Tensor input, Tensor scale_inv, int axis, int block_size, "
           "ScalarType dest_dtype) -> Tensor");
+    m.def("grouped_quantize_mxfp4_dual(Tensor input, Tensor group_lens, Tensor group_offs, "
+          "ScalarType dest_dtype, "
+          "bool rowwise_use_2d_block, bool rowwise_use_sr, bool rowwise_use_rht, "
+          "bool colwise_use_2d_block, bool colwise_use_sr, bool colwise_use_rht) -> Tensor[]");
 
     // ********* MXFP8 Quantization *********
     m.def("quantize_mxfp8_dual(Tensor input, ScalarType dest_dtype, "
@@ -67,15 +71,17 @@ TORCH_LIBRARY(primus_turbo_cpp_extension, m) {
           "bool rowwise_use_2d_block, bool colwise_use_2d_block, "
           "bool shuffle_rowwise_scale=False, bool shuffle_rowwise=False, "
           "bool shuffle_colwise_scale=False, bool shuffle_colwise=False) -> Tensor[]");
-    m.def("grouped_quantize_mxfp4_dual(Tensor input, Tensor group_lens, Tensor group_offs, "
-          "ScalarType dest_dtype, "
-          "bool rowwise_use_2d_block, bool rowwise_use_sr, bool rowwise_use_rht, "
-          "bool colwise_use_2d_block, bool colwise_use_sr, bool colwise_use_rht) -> Tensor[]");
+    m.def("grouped_quantize_mxfp8(Tensor input, Tensor group_lens, Tensor group_offs, "
+          "ScalarType dest_dtype, int axis, int padding_align_size, "
+          "bool use_2d_block, bool shuffle_scale=False, bool shuffle_out=False) -> Tensor[]");
     m.def("quantize_mxfp8(Tensor input, ScalarType dest_dtype, int axis, "
           "int padding_align_size, "
           "bool use_2d_block, bool shuffle_scale=False, bool shuffle_out=False) -> Tensor[]");
     m.def("dequantize_mxfp8(Tensor input, Tensor scale_inv, int axis, int block_size, "
           "ScalarType dest_dtype) -> Tensor");
+    m.def("grouped_dequantize_mxfp8(Tensor input, Tensor scale_inv, Tensor group_offs, "
+          "Tensor group_offs_padded, int axis, int block_size, ScalarType dest_dtype, "
+          "int? total_M=None) -> Tensor");
 
     // ********* Shuffle *********
     m.def("shuffle_scale(Tensor scale, int[] layout) -> Tensor");
@@ -134,13 +140,15 @@ TORCH_LIBRARY_IMPL(primus_turbo_cpp_extension, CUDA, m) {
     m.impl("quantize_mxfp4_dual", quantize_mxfp4_dual);
     m.impl("quantize_mxfp4", quantize_mxfp4);
     m.impl("dequantize_mxfp4", dequantize_mxfp4);
+    m.impl("grouped_quantize_mxfp4_dual", grouped_quantize_mxfp4_dual);
 
     // ********* MXFP8 Quantization *********
     m.impl("quantize_mxfp8_dual", quantize_mxfp8_dual);
     m.impl("grouped_quantize_mxfp8_dual", grouped_quantize_mxfp8_dual);
-    m.impl("grouped_quantize_mxfp4_dual", grouped_quantize_mxfp4_dual);
+    m.impl("grouped_quantize_mxfp8", grouped_quantize_mxfp8);
     m.impl("quantize_mxfp8", quantize_mxfp8);
     m.impl("dequantize_mxfp8", dequantize_mxfp8);
+    m.impl("grouped_dequantize_mxfp8", grouped_dequantize_mxfp8);
 
     // ********* Shuffle *********
     m.impl("shuffle_scale", shuffle_scale_impl);
@@ -181,13 +189,15 @@ TORCH_LIBRARY_IMPL(primus_turbo_cpp_extension, Meta, m) {
     m.impl("quantize_mxfp4_dual", quantize_mxfp4_dual_meta);
     m.impl("quantize_mxfp4", quantize_mxfp4_meta);
     m.impl("dequantize_mxfp4", dequantize_mxfp4_meta);
+    m.impl("grouped_quantize_mxfp4_dual", grouped_quantize_mxfp4_dual_meta);
 
     // ********* MXFP8 Quantization *********
     m.impl("quantize_mxfp8_dual", quantize_mxfp8_dual_meta);
     m.impl("grouped_quantize_mxfp8_dual", grouped_quantize_mxfp8_dual_meta);
-    m.impl("grouped_quantize_mxfp4_dual", grouped_quantize_mxfp4_dual_meta);
+    m.impl("grouped_quantize_mxfp8", grouped_quantize_mxfp8_meta);
     m.impl("quantize_mxfp8", quantize_mxfp8_meta);
     m.impl("dequantize_mxfp8", dequantize_mxfp8_meta);
+    m.impl("grouped_dequantize_mxfp8", grouped_dequantize_mxfp8_meta);
 
     // ********* Shuffle *********
     m.impl("shuffle_scale", shuffle_scale_impl_meta);
